2b8db2d566cf2e2da759bb8e70503d6d0ba202a1
zhenshen-mla <60142685+zhenshen-mla@users.noreply.github.com>
2020-08-16 20:54:54 +0800

    Add files via upload

---
+++ LogisticRegression/LogisticRegressionAPI.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+class LogisticRegressionGD(object):
+    """Logistic Regression Classifier using gradient descent.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    cost_ : list
+      Sum-of-squares cost function value in each epoch.
+
+    """
+
+    def __init__(self, eta=0.05, n_iter=100, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+          Training vectors, where n_samples is the number of samples and
+          n_features is the number of features.
+        y : array-like, shape = [n_samples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            print(output)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+
+            # note that we compute the logistic `cost` now
+            # instead of the sum of squared errors cost
+            cost = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output)))
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, z):
+        """Compute logistic sigmoid activation"""
+        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+        # equivalent to:
+        # return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
---
+++ LogisticRegression/LogisticRegressionTrain.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+from LogisticRegression.LogisticRegressionAPI import LogisticRegressionGD
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+from matplotlib.colors import ListedColormap
+
+def make_data():
+    iris = datasets.load_iris()
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=1, stratify=y)
+
+    return X_train, X_test, y_train, y_test
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolor='black')
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100,
+                    label='test set')
+
+
+if __name__ == '__main__':
+    X_train, X_test, y_train, y_test = make_data()
+    X_train_01_subset = X_train[(y_train == 0) | (y_train == 1)]
+    y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+
+    lrgd = LogisticRegressionGD(eta=0.05, n_iter=1000, random_state=1)
+    lrgd.fit(X_train_01_subset,
+             y_train_01_subset)
+
+    plot_decision_regions(X=X_train_01_subset,
+                          y=y_train_01_subset,
+                          classifier=lrgd)
+
+    plt.xlabel('petal length [standardized]')
+    plt.ylabel('petal width [standardized]')
+    plt.legend(loc='upper left')
+
+    plt.tight_layout()
+    # plt.savefig('images/03_05.png', dpi=300)
+    plt.show()
